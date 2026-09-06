a19e4080519eda5c0b4911fc70c3f321537973bc
mygraphicfairy <info@mygraphicfairy.com>
2026-09-06 16:56:18 -0400

    Make PM290 discovery more tolerant
    Updated Bluetooth device request to accept all devices and added validation for device name.

--- web/bridge/printer.js
+++ web/bridge/printer.js
@@ -432,11 +432,14 @@ export class PM290Printer {
     }
 
     this.device = await navigator.bluetooth.requestDevice({
-      filters: [
-        { namePrefix: "PM290" },
-      ],
-      optionalServices: [uuid(PM290_GATT_SERVICE & 0xffff)],
-    });
+  acceptAllDevices: true,
+  optionalServices: [uuid(PM290_GATT_SERVICE & 0xffff)],
+});
+    if (!this.device.name?.toUpperCase().startsWith("PM290")) {
+  throw new PrinterError(
+    `Selected device "${this.device.name ?? "Unknown"}" is not a PM290`
+  );
+}
 
     this.device.addEventListener("gattserverdisconnected", () => {
       this.log("PM290 disconnected");
